e8226f54da5a3e8f47ac5218b70b15a5eb8b7a5f
Artem Karimov <59018831+artemkarimov@users.noreply.github.com>
2020-06-15 22:12:33 +0300

    Update examples.js

--- JavaScript/examples.js
+++ JavaScript/examples.js
@@ -4,11 +4,54 @@ const exporto = require('./asynchronousQueue.js');
 
 const Queue = exporto.Queue;
 
+const color = exporto.color;
+
+const random = number => Math.floor(Math.random() * number);
+
+function User(user, company, priority) {
+  this.user = user;
+  this.company = company;
+  this.priority = priority;
+}
+
+const finished = () => {
+  console.log(color.green + 'Finished' + color.white);
+};
+
+const collectionOfUsers = (collection, numberOfUsers) => {
+  const companies = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numberOfCompanies = companies.length;
+  let counter = 1;
+  const arrayIterator = Array(numberOfUsers).keys();
+  const arrayOfNumbers = [...arrayIterator];
+  const priorities = arrayOfNumbers.map(value => value + 1);
+  for (let i = 1; i <= numberOfUsers; i++) {
+    const index = random(numberOfCompanies);
+    const tasks = [];
+    const numberOfTasks = random(3) + 1;
+    const length = priorities.length;
+    const indexOfPriority = random(length);
+    for (let j = 0; j < numberOfTasks; j++) {
+      tasks.push(`Task${counter}`);
+      counter++;
+    }
+    const user = new User(
+      i,
+      companies.charAt(index),
+      priorities[indexOfPriority]
+    );
+    priorities.splice(indexOfPriority, 1);
+    Object.setPrototypeOf(user, {});
+    collection.set(user, tasks);
+  }
+  return collection;
+};
+
+const map = new Map();
+
+collectionOfUsers(map, 8);
+
 const queue = new Queue();
-
-const map = exporto.map;
-
-const finished = exporto.finished;
 
 // FIRST CASE
 
@@ -20,8 +63,7 @@ for (const [key, value] of map.entries()) {
 
 // SECOND CASE
 
-queue.init()
-  .start(console.log, finished);
+queue.init().start(console.log, finished);
 
 // THIRD CASE
 
